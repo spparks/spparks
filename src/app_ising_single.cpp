89a9dfe28457ee604bc66fcb56d5cbc8efe679b3
sjplimp <sjplimp@685d9ffe-9724-0410-857b-ce06367cce3e>
2009-02-07 00:09:52 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/spparks/trunk@351 685d9ffe-9724-0410-857b-ce06367cce3e

---
+++ src/app_ising_single.cpp
@@ -0,0 +1,66 @@
+/* ----------------------------------------------------------------------
+   SPPARKS - Stochastic Parallel PARticle Kinetic Simulator
+   http://www.cs.sandia.gov/~sjplimp/spparks.html
+   Steve Plimpton, sjplimp@sandia.gov, Sandia National Laboratories
+
+   Copyright (2008) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPPARKS directory.
+------------------------------------------------------------------------- */
+
+#include "math.h"
+#include "app_ising_single.h"
+#include "random_park.h"
+
+using namespace SPPARKS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+AppIsingSingle::AppIsingSingle(SPPARKS *spk, int narg, char **arg) : 
+  AppIsing(spk,narg,arg)
+{
+  dt_sweep = 1.0;
+}
+
+/* ----------------------------------------------------------------------
+   perform a site event with null bin rejection
+   flip to opposite spin
+   null bin extends to size 1
+------------------------------------------------------------------------- */
+
+void AppIsingSingle::site_event_rejection(int i, RandomPark *random)
+{
+  int oldstate = lattice[i];
+  double einitial = site_energy(i);
+
+  // event = spin flip
+
+  if (oldstate == 1) lattice[i] = 2;
+  else lattice[i] = 1;
+  double efinal = site_energy(i);
+
+  // accept or reject via Boltzmann criterion
+
+  if (efinal <= einitial) {
+  } else if (temperature == 0.0) {
+    lattice[i] = oldstate;
+  } else if (random->uniform() > exp((einitial-efinal)*t_inverse)) {
+    lattice[i] = oldstate;
+  }
+
+  if (lattice[i] != oldstate) naccept++;
+
+  // set mask if site could not have changed
+  // if site changed, unset mask of sites with affected propensity
+  // OK to change mask of ghost sites since never used
+
+  if (Lmask) {
+    if (einitial < 0.5*numneigh[i]) mask[i] = 1;
+    if (lattice[i] != oldstate)
+      for (int j = 0; j < numneigh[i]; j++)
+	mask[neighbor[i][j]] = 0;
+  }
+}
---
+++ src/app_ising_single.h
@@ -0,0 +1,31 @@
+/* ----------------------------------------------------------------------
+   SPPARKS - Stochastic Parallel PARticle Kinetic Simulator
+   http://www.cs.sandia.gov/~sjplimp/spparks.html
+   Steve Plimpton, sjplimp@sandia.gov, Sandia National Laboratories
+
+   Copyright (2008) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level SPPARKS directory.
+------------------------------------------------------------------------- */
+
+#ifndef APP_ISING_SINGLE_H
+#define APP_ISING_SINGLE_H
+
+#include "app_ising.h"
+
+namespace SPPARKS_NS {
+
+class AppIsingSingle : public AppIsing {
+ public:
+  AppIsingSingle(class SPPARKS *, int, char **);
+  ~AppIsingSingle() {}
+
+  void site_event_rejection(int, class RandomPark *);
+};
+
+}
+
+#endif
