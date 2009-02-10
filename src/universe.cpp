5a5c2d4be9f48b026342f4c43bfc1a66a5597e3e
sjplimp <sjplimp@685d9ffe-9724-0410-857b-ce06367cce3e>
2009-02-10 16:32:53 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/spparks/trunk@357 685d9ffe-9724-0410-857b-ce06367cce3e

--- src/universe.cpp
+++ src/universe.cpp
@@ -25,7 +25,7 @@ using namespace SPPARKS_NS;
 
 Universe::Universe(SPPARKS *spk, MPI_Comm communicator) : Pointers(spk)
 {
-  version = (char *) "13 Nov 2008";
+  version = (char *) "10 Feb 2009";
 
   uworld = communicator;
   MPI_Comm_rank(uworld,&me);
