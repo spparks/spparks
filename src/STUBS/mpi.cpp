0887af1fe9536e706d814e6b06b33f52856f0729
sjplimp <sjplimp@685d9ffe-9724-0410-857b-ce06367cce3e>
2011-07-29 23:05:22 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/spparks/trunk@774 685d9ffe-9724-0410-857b-ce06367cce3e

--- src/STUBS/mpi.cpp
+++ src/STUBS/mpi.cpp
@@ -39,39 +39,60 @@ struct {
 /* MPI Functions */
 /* ---------------------------------------------------------------------- */
 
-void MPI_Init(int *argc, char ***argv) {}
+int MPI_Init(int *argc, char ***argv) {return 0;}
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Initialized(int *flag)
+int MPI_Initialized(int *flag)
 {
   *flag = 1;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Finalize() {}
+/* Returns "localhost" as the name of the processor */
+
+void MPI_Get_processor_name(char *name, int *resultlen)
+{
+  const char host[] = "localhost";
+  int len;
+
+  if (!name || !resultlen) return;
+
+  len = strlen(host);
+  memcpy(name,host,len+1);
+  *resultlen = len;
+  return;
+}
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Comm_rank(MPI_Comm comm, int *me)
+int MPI_Comm_rank(MPI_Comm comm, int *me)
 {
   *me = 0;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Comm_size(MPI_Comm comm, int *nprocs)
+int MPI_Comm_size(MPI_Comm comm, int *nprocs)
 {
   *nprocs = 1;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Abort(MPI_Comm comm, int errorcode)
+int MPI_Abort(MPI_Comm comm, int errorcode)
 {
   exit(1);
+  return 0;
 }
+
+/* ---------------------------------------------------------------------- */
+
+int MPI_Finalize() {return 0;}
 
 /* ---------------------------------------------------------------------- */
 
@@ -87,141 +108,171 @@ double MPI_Wtime()
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Send(void *buf, int count, MPI_Datatype datatype,
-	      int dest, int tag, MPI_Comm comm)
+int MPI_Type_size(MPI_Datatype datatype, int *size)
 {
-  printf("MPI Stub WARNING: Should not send message to self\n");
+  if (datatype == MPI_INT) *size = sizeof(int);
+  else if (datatype == MPI_FLOAT) *size = sizeof(float);
+  else if (datatype == MPI_DOUBLE) *size = sizeof(double);
+  else if (datatype == MPI_CHAR) *size = sizeof(char);
+  else if (datatype == MPI_BYTE) *size = sizeof(char);
+  else if (datatype == MPI_LONG_LONG) *size = sizeof(uint64_t);
+  else if (datatype == MPI_DOUBLE_INT) *size = sizeof(double_int);
+
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Rsend(void *buf, int count, MPI_Datatype datatype,
-	       int dest, int tag, MPI_Comm comm)
+int MPI_Send(void *buf, int count, MPI_Datatype datatype,
+             int dest, int tag, MPI_Comm comm)
+{
+  printf("MPI Stub WARNING: Should not send message to self\n");
+  return 0;
+}
+
+/* ---------------------------------------------------------------------- */
+
+int MPI_Rsend(void *buf, int count, MPI_Datatype datatype,
+              int dest, int tag, MPI_Comm comm)
 {
   printf("MPI Stub WARNING: Should not rsend message to self\n");
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Recv(void *buf, int count, MPI_Datatype datatype,
-	      int source, int tag, MPI_Comm comm, MPI_Status *status)
+int MPI_Recv(void *buf, int count, MPI_Datatype datatype,
+             int source, int tag, MPI_Comm comm, MPI_Status *status)
 {
   printf("MPI Stub WARNING: Should not recv message from self\n");
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Irecv(void *buf, int count, MPI_Datatype datatype,
-	       int source, int tag, MPI_Comm comm, MPI_Request *request)
+int MPI_Irecv(void *buf, int count, MPI_Datatype datatype,
+              int source, int tag, MPI_Comm comm, MPI_Request *request)
 {
   printf("MPI Stub WARNING: Should not recv message from self\n");
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Wait(MPI_Request *request, MPI_Status *status)
+int MPI_Wait(MPI_Request *request, MPI_Status *status)
 {
   printf("MPI Stub WARNING: Should not wait on message from self\n");
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Waitall(int n, MPI_Request *request, MPI_Status *status)
+int MPI_Waitall(int n, MPI_Request *request, MPI_Status *status)
 {
   printf("MPI Stub WARNING: Should not wait on message from self\n");
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Waitany(int count, MPI_Request *request, int *index, 
-		 MPI_Status *status)
+int MPI_Waitany(int count, MPI_Request *request, int *index, 
+                MPI_Status *status)
 {
   printf("MPI Stub WARNING: Should not wait on message from self\n");
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Sendrecv(void *sbuf, int scount, MPI_Datatype sdatatype,
-		  int dest, int stag, void *rbuf, int rcount,
-		  MPI_Datatype rdatatype, int source, int rtag,
-		  MPI_Comm comm, MPI_Status *status)
+int MPI_Sendrecv(void *sbuf, int scount, MPI_Datatype sdatatype,
+                 int dest, int stag, void *rbuf, int rcount,
+                 MPI_Datatype rdatatype, int source, int rtag,
+                 MPI_Comm comm, MPI_Status *status)
 {
   printf("MPI Stub WARNING: Should not send message to self\n");
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Get_count(MPI_Status *status, MPI_Datatype datatype, int *count)
+int MPI_Get_count(MPI_Status *status, MPI_Datatype datatype, int *count)
 {
   printf("MPI Stub WARNING: Should not get count of message to self\n");
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Comm_split(MPI_Comm comm, int color, int key, MPI_Comm *comm_out)
+int MPI_Comm_split(MPI_Comm comm, int color, int key, MPI_Comm *comm_out)
 {
   *comm_out = comm;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Comm_dup(MPI_Comm comm, MPI_Comm *comm_out)
+int MPI_Comm_dup(MPI_Comm comm, MPI_Comm *comm_out)
 {
   *comm_out = comm;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Comm_free(MPI_Comm *comm) { }
+int MPI_Comm_free(MPI_Comm *comm) {return 0;}
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Cart_create(MPI_Comm comm_old, int ndims, int *dims, int *periods,
-		     int reorder, MPI_Comm *comm_cart)
+int MPI_Cart_create(MPI_Comm comm_old, int ndims, int *dims, int *periods,
+                    int reorder, MPI_Comm *comm_cart)
 {
   *comm_cart = comm_old;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Cart_get(MPI_Comm comm, int maxdims, int *dims, int *periods,
-		  int *coords)
+int MPI_Cart_get(MPI_Comm comm, int maxdims, int *dims, int *periods,
+                 int *coords)
 {
   dims[0] = dims[1] = dims[2] = 1;
   periods[0] = periods[1] = periods[2] = 1;
   coords[0] = coords[1] = coords[2] = 0;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Cart_shift(MPI_Comm comm, int direction, int displ,
-		    int *source, int *dest)
+int MPI_Cart_shift(MPI_Comm comm, int direction, int displ,
+                   int *source, int *dest)
 {
   *source = *dest = 0;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Cart_rank(MPI_Comm comm, int *coords, int *rank)
+int MPI_Cart_rank(MPI_Comm comm, int *coords, int *rank)
 {
   *rank = 0;
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Barrier(MPI_Comm comm) {}
+int MPI_Barrier(MPI_Comm comm) {return 0;}
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Bcast(void *buf, int count, MPI_Datatype datatype,
-	       int root, MPI_Comm comm) {}
+int MPI_Bcast(void *buf, int count, MPI_Datatype datatype,
+              int root, MPI_Comm comm) {return 0;}
 
 /* ---------------------------------------------------------------------- */
 
 /* copy values from data1 to data2 */
 
-void MPI_Allreduce(void *sendbuf, void *recvbuf, int count,
-		   MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
+int MPI_Allreduce(void *sendbuf, void *recvbuf, int count,
+                  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
 {
   int n;
   if (datatype == MPI_INT) n = count*sizeof(int);
@@ -229,17 +280,18 @@ void MPI_Allreduce(void *sendbuf, void *recvbuf, int count,
   else if (datatype == MPI_DOUBLE) n = count*sizeof(double);
   else if (datatype == MPI_CHAR) n = count*sizeof(char);
   else if (datatype == MPI_BYTE) n = count*sizeof(char);
+  else if (datatype == MPI_LONG_LONG) n = count*sizeof(uint64_t);
   else if (datatype == MPI_DOUBLE_INT) n = count*sizeof(double_int);
-  else if (datatype == MPI_UNSIGNED_LONG) n = count*sizeof(uint64_t);
 
   memcpy(recvbuf,sendbuf,n);
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
 /* copy values from data1 to data2 */
 
-void MPI_Reduce(void *sendbuf, void *recvbuf, int count,
+int MPI_Reduce(void *sendbuf, void *recvbuf, int count,
 		   MPI_Datatype datatype, MPI_Op op,
 		   int root, MPI_Comm comm)
 {
@@ -249,16 +301,18 @@ void MPI_Reduce(void *sendbuf, void *recvbuf, int count,
   else if (datatype == MPI_DOUBLE) n = count*sizeof(double);
   else if (datatype == MPI_CHAR) n = count*sizeof(char);
   else if (datatype == MPI_BYTE) n = count*sizeof(char);
+  else if (datatype == MPI_LONG_LONG) n = count*sizeof(uint64_t);
   else if (datatype == MPI_DOUBLE_INT) n = count*sizeof(double_int);
-  else if (datatype == MPI_UNSIGNED_LONG) n = count*sizeof(uint64_t);
 
   memcpy(recvbuf,sendbuf,n);
+  return 0;
 }
+
 
 /* ---------------------------------------------------------------------- */
 
-void MPI_Scan(void *sendbuf, void *recvbuf, int count,
-	      MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
+int MPI_Scan(void *sendbuf, void *recvbuf, int count,
+             MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
 {
   int n;
   if (datatype == MPI_INT) n = count*sizeof(int);
@@ -266,19 +320,20 @@ void MPI_Scan(void *sendbuf, void *recvbuf, int count,
   else if (datatype == MPI_DOUBLE) n = count*sizeof(double);
   else if (datatype == MPI_CHAR) n = count*sizeof(char);
   else if (datatype == MPI_BYTE) n = count*sizeof(char);
+  else if (datatype == MPI_LONG_LONG) n = count*sizeof(uint64_t);
   else if (datatype == MPI_DOUBLE_INT) n = count*sizeof(double_int);
-  else if (datatype == MPI_UNSIGNED_LONG) n = count*sizeof(uint64_t);
 
   memcpy(recvbuf,sendbuf,n);
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
 /* copy values from data1 to data2 */
 
-void MPI_Allgather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
-		   void *recvbuf, int recvcount, MPI_Datatype recvtype,
-		   MPI_Comm comm)
+int MPI_Allgather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
+                  void *recvbuf, int recvcount, MPI_Datatype recvtype,
+                  MPI_Comm comm)
 {
   int n;
   if (sendtype == MPI_INT) n = sendcount*sizeof(int);
@@ -286,19 +341,20 @@ void MPI_Allgather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
   else if (sendtype == MPI_DOUBLE) n = sendcount*sizeof(double);
   else if (sendtype == MPI_CHAR) n = sendcount*sizeof(char);
   else if (sendtype == MPI_BYTE) n = sendcount*sizeof(char);
+  else if (sendtype == MPI_LONG_LONG) n = sendcount*sizeof(uint64_t);
   else if (sendtype == MPI_DOUBLE_INT) n = sendcount*sizeof(double_int);
-  else if (sendtype == MPI_UNSIGNED_LONG) n = sendcount*sizeof(uint64_t);
 
   memcpy(recvbuf,sendbuf,n);
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
 /* copy values from data1 to data2 */
 
-void MPI_Allgatherv(void *sendbuf, int sendcount, MPI_Datatype sendtype,
-		    void *recvbuf, int *recvcounts, int *displs,
-		    MPI_Datatype recvtype, MPI_Comm comm)
+int MPI_Allgatherv(void *sendbuf, int sendcount, MPI_Datatype sendtype,
+                   void *recvbuf, int *recvcounts, int *displs,
+                   MPI_Datatype recvtype, MPI_Comm comm)
 {
   int n;
   if (sendtype == MPI_INT) n = sendcount*sizeof(int);
@@ -306,18 +362,19 @@ void MPI_Allgatherv(void *sendbuf, int sendcount, MPI_Datatype sendtype,
   else if (sendtype == MPI_DOUBLE) n = sendcount*sizeof(double);
   else if (sendtype == MPI_CHAR) n = sendcount*sizeof(char);
   else if (sendtype == MPI_BYTE) n = sendcount*sizeof(char);
+  else if (sendtype == MPI_LONG_LONG) n = sendcount*sizeof(uint64_t);
   else if (sendtype == MPI_DOUBLE_INT) n = sendcount*sizeof(double_int);
-  else if (sendtype == MPI_UNSIGNED_LONG) n = sendcount*sizeof(uint64_t);
 
   memcpy(recvbuf,sendbuf,n);
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
 /* copy values from data1 to data2 */
 
-void MPI_Reduce_scatter(void *sendbuf, void *recvbuf, int *recvcounts,
-			MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
+int MPI_Reduce_scatter(void *sendbuf, void *recvbuf, int *recvcounts,
+                       MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
 {
   int n;
   if (datatype == MPI_INT) n = *recvcounts*sizeof(int);
@@ -325,19 +382,20 @@ void MPI_Reduce_scatter(void *sendbuf, void *recvbuf, int *recvcounts,
   else if (datatype == MPI_DOUBLE) n = *recvcounts*sizeof(double);
   else if (datatype == MPI_CHAR) n = *recvcounts*sizeof(char);
   else if (datatype == MPI_BYTE) n = *recvcounts*sizeof(char);
+  else if (datatype == MPI_LONG_LONG) n = *recvcounts*sizeof(uint64_t);
   else if (datatype == MPI_DOUBLE_INT) n = *recvcounts*sizeof(double_int);
-  else if (datatype == MPI_UNSIGNED_LONG) n = *recvcounts*sizeof(uint64_t);
 
   memcpy(recvbuf,sendbuf,n);
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
 /* copy values from data1 to data2 */
 
-void MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
-		   void *recvbuf, int recvcount, MPI_Datatype recvtype,
-		   int root, MPI_Comm comm)
+int MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
+               void *recvbuf, int recvcount, MPI_Datatype recvtype,
+               int root, MPI_Comm comm)
 {
   int n;
   if (sendtype == MPI_INT) n = sendcount*sizeof(int);
@@ -345,17 +403,18 @@ void MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
   else if (sendtype == MPI_DOUBLE) n = sendcount*sizeof(double);
   else if (sendtype == MPI_CHAR) n = sendcount*sizeof(char);
   else if (sendtype == MPI_BYTE) n = sendcount*sizeof(char);
+  else if (sendtype == MPI_LONG_LONG) n = sendcount*sizeof(uint64_t);
   else if (sendtype == MPI_DOUBLE_INT) n = sendcount*sizeof(double_int);
-  else if (sendtype == MPI_UNSIGNED_LONG) n = sendcount*sizeof(uint64_t);
 
   memcpy(recvbuf,sendbuf,n);
+  return 0;
 }
 
 /* ---------------------------------------------------------------------- */
 
 /* copy values from data1 to data2 */
 
-void MPI_Gatherv(void *sendbuf, int sendcount, MPI_Datatype sendtype,
+int MPI_Gatherv(void *sendbuf, int sendcount, MPI_Datatype sendtype,
 		    void *recvbuf, int *recvcounts, int *displs,
 		    MPI_Datatype recvtype, int root, MPI_Comm comm)
 {
@@ -365,8 +424,9 @@ void MPI_Gatherv(void *sendbuf, int sendcount, MPI_Datatype sendtype,
   else if (sendtype == MPI_DOUBLE) n = sendcount*sizeof(double);
   else if (sendtype == MPI_CHAR) n = sendcount*sizeof(char);
   else if (sendtype == MPI_BYTE) n = sendcount*sizeof(char);
+  else if (sendtype == MPI_LONG_LONG) n = sendcount*sizeof(uint64_t);
   else if (sendtype == MPI_DOUBLE_INT) n = sendcount*sizeof(double_int);
-  else if (sendtype == MPI_UNSIGNED_LONG) n = sendcount*sizeof(uint64_t);
 
   memcpy(recvbuf,sendbuf,n);
+  return 0;
 }
