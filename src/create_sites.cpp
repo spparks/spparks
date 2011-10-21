0ed5fc8b3dfa14d7174c9f68d551685dbf9bedcf
athomps <athomps@685d9ffe-9724-0410-857b-ce06367cce3e>
2011-10-21 21:48:29 +0000

    Bug found by Enrique Saez and colleagues
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/spparks/trunk@811 685d9ffe-9724-0410-857b-ce06367cce3e

--- src/create_sites.cpp
+++ src/create_sites.cpp
@@ -280,7 +280,7 @@ void CreateSites::structured_lattice()
   }
 
   if (zperiodic) {
-    zorig = boxxlo;
+    zorig = boxzlo;
     zlo = 0;
     zhi = nz-1;
   } else {
@@ -388,9 +388,8 @@ void CreateSites::structured_connectivity()
   int i,j,m,max;
   int ineigh,jneigh,kneigh,mneigh;
   tagint gid;
-  double x,y,z,xneigh,yneigh,zneigh;
+  double xneigh,yneigh,zneigh;
 
-  int dimension = domain->dimension;
   int nonperiodic = domain->nonperiodic;
   int xperiodic = domain->xperiodic;
   int yperiodic = domain->yperiodic;
@@ -510,9 +509,9 @@ void CreateSites::structured_connectivity()
       // discard neighs that are outside non-periodic box or region
 
       if (nonperiodic) {
-	if (!xperiodic && (xneigh < boxxlo || xneigh >= boxxhi)) continue;
-	if (!yperiodic && (yneigh < boxylo || yneigh >= boxyhi)) continue;
-	if (!zperiodic && (zneigh < boxzlo || zneigh >= boxzhi)) continue;
+        if (!xperiodic && (xneigh < boxxlo || xneigh >= boxxhi)) continue;
+        if (!yperiodic && (yneigh < boxylo || yneigh >= boxyhi)) continue;
+        if (!zperiodic && (zneigh < boxzlo || zneigh >= boxzhi)) continue;
       }
       if (style == REGION &&
 	  domain->regions[nregion]->match(xneigh,yneigh,zneigh) == 0) continue;
@@ -520,8 +519,8 @@ void CreateSites::structured_connectivity()
       // gid = global ID of neighbor
       // calculated in same manner that structured_lattice() generated IDs
 
-      gid = (kneigh-zlo)*(yhi-ylo+1)*(xhi-xlo+1) + (jneigh-ylo)*(xhi-xlo+1) +
-	(ineigh-xlo) + mneigh + 1;
+      gid = (kneigh-zlo)*(yhi-ylo+1)*(xhi-xlo+1)*nbasis + (jneigh-ylo)*(xhi-xlo+1)*nbasis +
+	(ineigh-xlo)*nbasis + mneigh + 1;
 
       //printf("EEE %d %d: %d\n",i,j,gid);
 
