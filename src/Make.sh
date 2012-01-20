e6bd44b92242c8bd0571a4c5b60bb7bf48f9d800
sjplimp <sjplimp@685d9ffe-9724-0410-857b-ce06367cce3e>
2012-01-20 16:55:36 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/spparks/trunk@851 685d9ffe-9724-0410-857b-ce06367cce3e

---
+++ src/Make.sh
@@ -0,0 +1,70 @@
+# Make.sh = update Makefile.lib or Makefile.list or style_*.h files
+# Syntax: sh Make.sh style
+#         sh Make.sh Makefile.lib
+#         sh Make.sh Makefile.list
+
+# function to create one style_*.h file
+# must whack *.d files that depend on style_*.h file,
+# else Make will not recreate them
+
+style () {
+  list=`grep -l $1 $2*.h`
+  if (test -e style_$3.tmp) then
+    rm -f style_$3.tmp
+  fi
+  for file in $list; do
+    qfile="\"$file\""
+    echo "#include $qfile" >> style_$3.tmp
+  done
+  if (test ! -e style_$3.tmp) then
+    rm -f style_$3.h
+    touch style_$3.h
+  elif (test ! -e style_$3.h) then
+    mv style_$3.tmp style_$3.h
+    rm -f Obj_*/$4.d
+    rm -f Obj_*/lammps.d
+  elif (test "`diff --brief style_$3.h style_$3.tmp`" != "") then
+    mv style_$3.tmp style_$3.h
+    rm -f Obj_*/$4.d
+    rm -f Obj_*/lammps.d
+  else
+    rm -f style_$3.tmp
+  fi
+}
+
+# create individual style files
+# called by "make machine"
+
+if (test $1 = "style") then
+
+  style APP_CLASS       app_        app        input
+  style COMMAND_CLASS   ""          command    input
+  style DIAG_CLASS      diag_       diag       input
+  style DUMP_CLASS      dump_       dump       output
+  style PAIR_CLASS      pair_       pair       potential
+  style REGION_CLASS    region_     region     domain
+  style SOLVE_CLASS     solve_      solve      input
+
+# edit Makefile.lib
+# called by "make makelib"
+# use current list of *.cpp and *.h files in src dir w/out main.cpp
+
+elif (test $1 = "Makefile.lib") then
+
+  list=`ls -1 *.cpp | sed s/^main\.cpp// | tr "[:cntrl:]" " "`
+  sed -i -e "s/SRC =	.*/SRC =	$list/" Makefile.lib
+  list=`ls -1 *.h | tr "[:cntrl:]" " "`
+  sed -i -e "s/INC =	.*/INC =	$list/" Makefile.lib
+
+# edit Makefile.list
+# called by "make makelist"
+# use current list of *.cpp and *.h files in src dir
+
+elif (test $1 = "Makefile.list") then
+
+  list=`ls -1 *.cpp | tr "[:cntrl:]" " "`
+  sed -i -e "s/SRC =	.*/SRC =	$list/" Makefile.list
+  list=`ls -1 *.h | tr "[:cntrl:]" " "`
+  sed -i -e "s/INC =	.*/INC =	$list/" Makefile.list
+
+fi
