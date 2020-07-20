765babb56966ecf28653ffc5cec1d4cfbab14c29
sjplimp <sjplimp@685d9ffe-9724-0410-857b-ce06367cce3e>
2020-07-20 20:15:03 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/spparks/trunk@1249 685d9ffe-9724-0410-857b-ce06367cce3e

--- lib/stitch/Install.py
+++ lib/stitch/Install.py
@@ -10,25 +10,21 @@ import sys,os,re,subprocess
 
 help = """
 Syntax from src dir: make lib-stitch args="-b"
-                 or: make lib-stitch args="-p /usr/local/stitch-1.0"
 Syntax from lib dir: python Install.py -b
-                 or: python Install.py -p /usr/local/stitch-1.0
 
-specify one or more options, order does not matter
+specify option
 
-  -b = download and build the Stitch library
-  -p = specify folder of existing Stitch installation
+  -b = build the Stitch library and create links to it
 
 Example:
 
-make lib-stitch args="-b"   # download/build in lib/stitch/stitch-1.0
-make lib-stitch args="-p $HOME/stitch-1.0" # use existing Stitch installation in $HOME/stitch-1.0
+make lib-stitch args="-b"   # build in lib/stitch and set links
 """
 
 # settings
 
 version = "stitch-1.0"
-url = "URL for Stitch library tarball when avaiable for download" % version
+url = "URL for Stitch library tarball when avaiable for download %s" % version
 
 # print error message or help
 
@@ -60,29 +56,6 @@ def which(program):
 
   return None
 
-def geturl(url,fname):
-  success = False
-
-  if which('curl') != None:
-    cmd = 'curl -L -o "%s" %s' % (fname,url)
-    try:
-      subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
-      success = True
-    except subprocess.CalledProcessError as e:
-      print("Calling curl failed with: %s" % e.output.decode('UTF-8'))
-
-  if not success and which('wget') != None:
-    cmd = 'wget -O "%s" %s' % (fname,url)
-    try:
-      subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
-      success = True
-    except subprocess.CalledProcessError as e:
-      print("Calling wget failed with: %s" % e.output.decode('UTF-8'))
-
-  if not success:
-    error("Failed to download source code with 'curl' or 'wget'")
-  return
-
 # parse args
 
 args = sys.argv[1:]
@@ -90,65 +63,19 @@ nargs = len(args)
 if nargs == 0: error()
 
 homepath = "."
-homedir = version
 
-buildflag = False
-pathflag = False
+buildflag = True
 linkflag = True
 
-iarg = 0
-while iarg < nargs:
-  if args[iarg] == "-v":
-    if iarg+2 > nargs: error()
-    version = args[iarg+1]
-    iarg += 2
-  elif args[iarg] == "-p":
-    if iarg+2 > nargs: error()
-    stitchpath = fullpath(args[iarg+1])
-    pathflag = True
-    iarg += 2
-  elif args[iarg] == "-b":
-    buildflag = True
-    iarg += 1
-  else: error()
-
+version = "libstitch"
 homepath = fullpath(homepath)
 homedir = "%s/%s" % (homepath,version)
-
-if (pathflag):
-    if not os.path.isdir(stitchpath): error("Stitch path does not exist")
-    homedir = stitchpath
-
-if (buildflag and pathflag):
-    error("Cannot use -b and -p flag at the same time")
-
-if (not buildflag and not pathflag):
-    error("Have to use either -b or -p flag")
-
-# download and unpack Stitch tarball
-
-#if buildflag:
-#  print("Downloading Stitch ...")
-#  geturl(url,"%s/%s.tar.gz" % (homepath,version))
-#
-#  print("Unpacking Stitch tarball ...")
-#  if os.path.exists("%s/%s" % (homepath,version)):
-#    cmd = 'rm -rf "%s/%s"' % (homepath,version)
-#    subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
-#  cmd = 'cd "%s"; tar -xzvf %s.tar.gz' % (homepath,version)
-#  subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
-#  os.remove("%s/%s.tar.gz" % (homepath,version))
-#  if os.path.basename(homedir) != version:
-#    if os.path.exists(homedir):
-#      cmd = 'rm -rf "%s"' % homedir
-#      subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
-#    os.rename("%s/%s" % (homepath,version),homedir)
 
 # build Stitch
 
 if buildflag:
   print("Building Stitch ...")
-  cmd = "cd %s; make -f Makefile.steve stitch.lib" % homedir
+  cmd = "cd %s; make" % homedir
   txt = subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
   print(txt.decode('UTF-8'))
 
