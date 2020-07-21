b2377d5f51c226e266e38712caf8d3548ba10cec
jamitch <jamitch@685d9ffe-9724-0410-857b-ce06367cce3e>
2020-07-21 14:08:52 +0000

    Add 'copyright' and 'license' information to stitch source files
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/spparks/trunk@1255 685d9ffe-9724-0410-857b-ce06367cce3e

--- lib/stitch/libstitch/setup.py
+++ lib/stitch/libstitch/setup.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 # encoding: utf-8
+# Copyright 2019 National Technology & Engineering Solutions of
+# Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525
+# with NTESS, the U.S. Government retains certain rights in this software.
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+# 
+# For more information, contact Jay Lofstead (gflofst@sandeia.gov) or
+# John Mitchell (jamitch@sandia.gov) for more information.
+
 from os.path import join
 
 def get_libraries_list(lapack_info):
@@ -44,11 +65,13 @@ def configuration(parent_package='',top_path=None):
         try:
             extra_compile_args=lapack['extra_compile_args']
         except KeyError:
-            extra_compile_args=None
+            #extra_compile_args=None
+            pass
         try:
             extra_link_args=lapack['extra_link_args']
         except KeyError:
-            extra_link_args=None
+            #extra_link_args=None
+            pass
 
     # package
     libraries=get_libraries_list(lapack);
