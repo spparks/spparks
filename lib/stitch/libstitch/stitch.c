6f7f83512785d08a2f7c11dcc1699f1a6b1e4431
jamitch <jamitch@685d9ffe-9724-0410-857b-ce06367cce3e>
2020-07-21 14:08:52 +0000

    Add 'copyright' and 'license' information to stitch source files
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/spparks/trunk@1255 685d9ffe-9724-0410-857b-ce06367cce3e
    
    Former-commit-id: 9ea5813137f86e1ab5ed51818ea1dc12bc21f95c

--- lib/stitch/libstitch/stitch.c
+++ lib/stitch/libstitch/stitch.c
@@ -1,3 +1,24 @@
+/* Copyright 2019 National Technology & Engineering Solutions of
+ * Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525
+ * with NTESS, the U.S. Government retains certain rights in this software.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * 
+ * For more information, contact Jay Lofstead (gflofst@sandeia.gov) or
+ * John Mitchell (jamitch@sandia.gov) for more information.
+ */ 
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
--- lib/stitch/setup.py
+++ lib/stitch/setup.py
@@ -1,3 +1,24 @@
+# Copyright 2019 National Technology & Engineering Solutions of
+# Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525
+# with NTESS, the U.S. Government retains certain rights in this software.
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+# 
+# For more information, contact Jay Lofstead (gflofst@sandeia.gov) or
+# John Mitchell (jamitch@sandia.gov) for more information.
+
 import os
 from os.path import join
 
@@ -16,8 +37,13 @@ def configuration(parent_package='',top_path=None):
                                  sources=[join('libstitch',x) for x in sources],
                                  install_dir='stitch/lib',
                                  build_info=dict(extra_compiler_args=['-std=gnu99']))
+    # TODO 
+    # Figure out how to configure one of these to better document installation of libstitch
+    #config.add_npy_pkg_config('libstitch/libstitch.ini.in','stitch/libstitch',subst_dict={'foo':'stitch'})
     return config
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
     setup(**configuration(top_path='').todict())
+
+      
