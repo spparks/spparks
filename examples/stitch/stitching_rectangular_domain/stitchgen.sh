c492c31a83b5bbf346da23ce9ca99a0e048aa635
jamitch <jamitch@sandia.gov>
2020-09-11 14:19:50 -0600

    Incremental work on 'rectangular domain' stitching

--- examples/stitch/stitching_rectangular_domain/stitchgen.sh
+++ examples/stitch/stitching_rectangular_domain/stitchgen.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 PATHDATA=stitching.dat
+declare -a THICKNESS
+declare -a LAYER_PTR
 declare -a PATHS
 declare -a WINDOWS
 declare -a LAYER_WINDOWS
 let ZSTART=0
-let DEPTH_HAZ=5
+let DEPTH_HAZ=6
 
 
-function get_paths_and_windows {
+function get_thicknesses_paths_and_windows {
 
 local F
 local var
@@ -16,24 +18,28 @@ local Z0
 let Z1=$ZSTART
 local LAYER_BOTTOM
 
-let local L=1
+let local L=0
+let local P=0
+let local PTR=0
 while read LINE; do
 
   F=$(echo $LINE | cut -d ' ' -f1)
-  echo $F
+  #echo $F
 
   case "$F" in
 
     layer)
       var=$(echo $LINE | cut -d ' ' -f2)
       if [ "$var" == "thickness" ]; then
-        let THICKNESS=$(echo $LINE | cut -d ' ' -f3)
+	THICKNESS+=($(echo $LINE | cut -d ' ' -f3))
+	LAYER_PTR+=($PTR)
+        let Z1=$Z1+${THICKNESS[$L]}
+        let LAYER_BOTTOM=${Z1}-${THICKNESS[$L]}
       else
 	echo "INPUT ERROR: expected 'layer thickness'"
         exit 1
       fi
-      let Z1=$Z1+$THICKNESS
-      if [ $L -ge 2 ]; then
+      if [ $L -ge 1 ]; then
 	let Z0=$Z1-$DEPTH_HAZ
       else
 	let Z0=$ZSTART
@@ -61,13 +67,17 @@ while read LINE; do
 	echo "INPUT ERROR: expected 'block'"
         exit 1
       fi
-      let LAYER_BOTTOM=${Z1}-${THICKNESS}
       
       LAYER_WINDOWS+=("$(echo $LINE | cut -d ' ' -f 9,10,11,12) ${LAYER_BOTTOM} ${Z1}")
       WINDOWS+=("$(echo $LINE | cut -d ' ' -f 9,10,11,12) ${Z0} ${Z1}")
+      #echo "PROCESS Layer # ${L}; Z0=${Z0}; Z1=${Z1}; LAYER_BOTTOM=${LAYER_BOTTOM}"
+      #echo "PROCESS Layer # ${L}; LAYER_WINDOW=${LAYER_WINDOWS[$PTR]}"
+
+      # Now move PTR in preparation for next PATH
+      let PTR+=1
       ;;
-      \#)
-         echo COMMENT: ${LINE}
+    \#)
+         #echo COMMENT: ${LINE}
       ;;
     *)
       echo "Usage: {layer|domain}"
@@ -78,25 +88,39 @@ while read LINE; do
 
 done < $1
 
+# Add one extra layer pointer to start of non existent next layer
+LAYER_PTR+=($PTR)
+
 }
 
+get_thicknesses_paths_and_windows $PATHDATA
+NUM_LAYERS=${#THICKNESS[@]}
 
-get_paths_and_windows $PATHDATA
+echo "Number of layers = ${NUM_LAYERS}"
+echo "Layer thicknesses = ${THICKNESS[@]}"
+echo "Layer PTR = ${LAYER_PTR[@]}"
 
-NUM_RUNS=${#PATHS[@]}
 
-for (( r=0; r<$NUM_RUNS; r++ )); do
-  echo LAYER_WINDOW=${LAYER_WINDOWS[$r]}
-  echo WINDOW=${WINDOWS[$r]}
-  echo PATH=${PATHS[$r]}
+let local PTR=0
+for (( L=0; L<$NUM_LAYERS; L++ )); do
+  
+  # Number of paths on layer
+  let LAYNUM=$L+1
+  let NUM_PATHS=${LAYER_PTR[$L+1]}-${LAYER_PTR[$L]}
+  echo "Layer # ${LAYNUM}; Number of paths on layer = ${NUM_PATHS}"
+  for (( P=${LAYER_PTR[$L]}; P<${LAYER_PTR[$L+1]}; P++ )); do
+    echo "LAYER_WINDOW=${LAYER_WINDOWS[$P]}"
+    echo "WINDOW=${WINDOWS[$P]}"
+    echo "PATH=${PATHS[$P]}"
+    #####################################
+    # Run potts model to initialize layer
+    cat in.init | sed s"/WINDOW/${WINDOW}/" > in.potts_init
+    
+    #  
+    # Run SPPARKS to initialize microstructure on layer
+    SEED=$RANDOM
+    $SPPARKS -var SEED $SEED < in.potts_init
+    #####################################
+  done
 
-    # Run potts model to initialize first layer
-    if [ $L -eq 1 ]; then
-       cat in.init | sed s"/WINDOW/${WINDOW}/" > in.potts_init
-       
-       #  
-       # Run SPPARKS to initialize microstructure on layer
-       SEED=$RANDOM
-       $SPPARKS -var SEED $SEED < in.potts_init
-    fi
 done
