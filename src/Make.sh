b5311379fb035bb6e82da34b021695f219aeb87d
John A. Mitchell <jamitch@sandia.gov>
2022-09-08 17:21:47 -0600

    Remove old/un-used options from Make.sh

--- src/Make.sh
+++ src/Make.sh
@@ -1,9 +1,5 @@
-# Make.sh = update Makefile.lib, Makefile.shlib, Makefile.list 
-#           or style_*.h files
+# Make.sh = update  style_*.h files
 # Syntax: sh Make.sh style
-#         sh Make.sh Makefile.lib
-#         sh Make.sh Makefile.shlib
-#         sh Make.sh Makefile.list
 
 # function to create one style_*.h file
 # must whack *.d files that depend on style_*.h file,
@@ -35,7 +31,6 @@ style () {
 }
 
 # create individual style files
-# called by "make machine"
 
 if (test $1 = "style") then
 
@@ -46,38 +41,5 @@ if (test $1 = "style") then
   style PAIR_CLASS      pair_       pair       potential
   style REGION_CLASS    region_     region     domain
   style SOLVE_CLASS     solve_      solve      input
-
-# edit Makefile.lib, for creating non-shared lib
-# called by "make makelib"
-# use current list of *.cpp and *.h files in src dir w/out main.cpp
-
-elif (test $1 = "Makefile.lib") then
-
-  list=`ls -1 *.cpp | sed s/^main\.cpp// | tr "[:cntrl:]" " "`
-  sed -i -e "s/SRC =	.*/SRC =	$list/" Makefile.lib
-  list=`ls -1 *.h | tr "[:cntrl:]" " "`
-  sed -i -e "s/INC =	.*/INC =	$list/" Makefile.lib
-
-# edit Makefile.shlib, for creating shared lib
-# called by "make makeshlib"
-# use current list of *.cpp and *.h files in src dir w/out main.cpp
-
-elif (test $1 = "Makefile.shlib") then
-
-  list=`ls -1 *.cpp | sed s/^main\.cpp// | tr "[:cntrl:]" " "`
-  sed -i -e "s/SRC =	.*/SRC =	$list/" Makefile.shlib
-  list=`ls -1 *.h | tr "[:cntrl:]" " "`
-  sed -i -e "s/INC =	.*/INC =	$list/" Makefile.shlib
-
-# edit Makefile.list
-# called by "make makelist"
-# use current list of *.cpp and *.h files in src dir
-
-elif (test $1 = "Makefile.list") then
-
-  list=`ls -1 *.cpp | tr "[:cntrl:]" " "`
-  sed -i -e "s/SRC =	.*/SRC =	$list/" Makefile.list
-  list=`ls -1 *.h | tr "[:cntrl:]" " "`
-  sed -i -e "s/INC =	.*/INC =	$list/" Makefile.list
 
 fi
